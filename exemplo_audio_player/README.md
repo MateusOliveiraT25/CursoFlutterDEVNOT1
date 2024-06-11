171150a9bc1ea08f69ad7de1f377747b8f71d44e
Mateus Oliveira <123783867+MateusOliveiraT25@users.noreply.github.com>
2024-06-11 20:11:02 -0300

    11/06/24

---
+++ exemplo_audio_player/README.md
@@ -0,0 +1,11 @@
+# exemplo_audio_player
+
+A new Flutter project.
+Exemplo_audio_player
+    Model
+        audioModel
+    Service
+        audioService (lista de audios - Json)
+    Screen
+        HomeScreen - View Listav de audios
+        AudioPlayerScreen - Player de audios(biblioteca)
---
+++ exemplo_audio_player/lib/Services/audio_service.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../Models/audio_model.dart';
+import 'dart:convert';
+
+
+class AudioService{
+    List<AudioModel> _list = [];
+  List<AudioModel> get list => _list;
+
+//url da api
+  final String url = 'http://10.109.204.28:3000/audios';
+
+  //fetch da lista de audio
+
+ Future<void> fetchAudio() async{
+    final response = await http.get(Uri.parse(url));
+    if(response.statusCode==200){
+      final data = json.decode(response.body);
+      _list = data.map((e)=> AudioModel.fromMap(e)).toList();
+    }
+  }
+}
